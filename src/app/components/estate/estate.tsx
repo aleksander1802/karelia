bd5674a8b371e3463df220724103915b26d3aa7e
Alex2 <aleksander1802@gmail.com>
2023-12-28 20:34:08 +0300

    feat: first block is ready

---
+++ src/app/components/estate/estate.tsx
@@ -0,0 +1,188 @@
+import Image from 'next/image';
+import { Poiret_One } from 'next/font/google';
+import Logo from '@/app/assets/svg/Logo-svg';
+import LogoLink from '@/app/assets/svg/Logo-link-svg';
+
+const poiret = Poiret_One({ subsets: ['cyrillic'], weight: '400' });
+
+export default function Estate() {
+	const estate =
+		'Уютная усадьба в атмосфере первозданной природы, укрытая природой от окружения';
+
+	return (
+		<div className="estate relative min-h-screen bg-cover bg-no-repeat bg-center text-3xl cursor-default">
+			<div className="p-[220px] pt-[331px] pb-0">
+				<div className="inline-flex justify-end items-center max-w-[418px] h-[78px] text-[20px] text-font_light leading-[140%] border-l-2 pl-[35px]">
+					{estate}
+				</div>
+				<div className="text-[198px] pt-[370px] pb-[138px] leading-[95px] -tracking-[5px] font-mirra flex justify-between ">
+					<span>Усадьба</span>
+					<span className="relative">
+						в
+						<div className="absolute top-1/2 left-1/2 -translate-x-[52%] -translate-y-[57%] max-w-max">
+							<Logo width="354" />
+						</div>
+					</span>
+					<span>карелии</span>
+				</div>
+
+				<form className="flex justify-between flex-row justify-items-center items-center h-[81px] bg-primary [&>*]:w-[357px] ">
+					<span className="font-mirra text-secondary flex pt-[23px] pr-[38px] pb-[12px] pl-[45px] justify-end items-center text-[66px] leading-[90px]">
+						Арендовать
+					</span>
+
+					<input
+						type="text"
+						name="name"
+						id="name"
+						placeholder="Ваше имя"
+						className="h-[74px] bg-primary border-l-[1px] border-l-secondary outline-none  py-[22px] px-[37px] text-[20px]"
+					/>
+
+					<input
+						type="number"
+						name="number"
+						id="number"
+						placeholder="Телефон"
+						className="h-[74px] bg-primary border-l-[1px] border-l-secondary outline-none input-number py-[22px] px-[37px] text-[20px]"
+					/>
+
+					<button
+						type="submit"
+						className="flex justify-center items-center text-[24px] font-light bg-accent h-[81px] py-[26px] px-[116px] hover:bg-accent_light"
+					>
+						Отправить
+					</button>
+				</form>
+
+				<div
+					className={`flex flex-wrap  pt-[265px] [&>*]:w-max-content [&>*]:h-[96px]  [&>div]:flex [&>div]:flex-col [&>div]:justify-between  gap-x-[144px] gap-y-[131px]
+					font-extralight not-italic`}
+				>
+					<div className="pr-[18px]">
+						<div>
+							<span className={`${poiret.className} text-[65px]`}>
+								0,5
+							</span>
+							<span
+								className={`${poiret.className} text-[65px] pl-[10px]`}
+							>
+								га
+							</span>
+						</div>
+
+						<span className="text-secondary text-[24px]">
+							Площадь усадьбы
+						</span>
+					</div>
+					<div>
+						<div>
+							<span className={`${poiret.className} text-[65px]`}>
+								0,25
+							</span>
+							<span
+								className={`${poiret.className} text-[65px] pl-[10px]`}
+							>
+								га
+							</span>
+						</div>
+						<span className="text-secondary text-[24px]">
+							Собственный берег
+						</span>
+					</div>
+					<div className="pr-[18px]">
+						<div>
+							<span
+								className={`${poiret.className} text-[65px] `}
+							>
+								200
+							</span>
+							<span
+								className={`${poiret.className} text-[65px] pl-[10px] relative`}
+							>
+								м
+								<span className="text-[30px] absolute top-[3px]">
+									2
+								</span>{' '}
+							</span>
+						</div>
+
+						<span className="text-secondary text-[24px]">
+							Master House
+						</span>
+					</div>
+					<div>
+						<div>
+							<span
+								className={`${poiret.className} text-[65px] `}
+							>
+								150
+							</span>
+							<span
+								className={`${poiret.className} text-[65px] pl-[10px] relative`}
+							>
+								м
+								<span className="text-[30px] absolute top-[3px]">
+									2
+								</span>{' '}
+							</span>
+						</div>
+
+						<span className="text-secondary text-[24px]">
+							Guest House
+						</span>
+					</div>
+					<div>
+						<div>
+							<span
+								className={`${poiret.className} text-[65px] `}
+							>
+								230
+							</span>
+							<span
+								className={`${poiret.className} text-[65px] pl-[10px] relative`}
+							>
+								м
+								<span className="text-[30px] absolute top-[3px]">
+									2
+								</span>{' '}
+							</span>
+						</div>
+
+						<span className="text-secondary text-[24px]">
+							Spa house
+						</span>
+					</div>
+				</div>
+
+				<div className="pt-[207px] w-[948px] font-mirra text-[150px] leading-[121px] text-secondary">
+					В вашем распоряжении
+				</div>
+			</div>
+
+			<div className="absolute bottom-0 right-1/2 translate-x-[50%] -translate-y-[90%] z-30">
+				<LogoLink
+					color="#805140"
+					width="230"
+				/>
+			</div>
+
+			<Image
+				src={'/images/estate/tree.png'}
+				width={1120}
+				height={797}
+				alt="tree"
+				priority={true}
+				className="absolute bottom-0 right-0 translate-x-[16%] translate-y-[10%] z-20"
+			/>
+			<Image
+				src={'/images/estate/tree_back.png'}
+				width={1120}
+				height={797}
+				alt="tree"
+				priority={true}
+				className="absolute bottom-0 right-0 translate-x-[16%] translate-y-[30%] z-10"
+			/>
+		</div>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '960px',
+			xl: '1400px',
+		},
+		extend: {
+			colors: {
+				primary: '#090e17',
+				secondary: '#858AB5',
+				accent: '#805140',
+				accent_light: '#aa6d57',
+				font_light: '#E6E6E6',
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			fontFamily: {
+				mirra: [`Mirra Auriolic`, 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
